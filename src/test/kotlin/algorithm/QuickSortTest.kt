39821ce13af138da1a339d1b2cde70a3cc2d2835
Anton Marin <antonaryo85@gmail.com>
2022-07-26 08:21:48 +0300

    quicksort algorithm

--- src/test/kotlin/algorithm/QuickSortTest.kt
+++ src/test/kotlin/algorithm/QuickSortTest.kt
@@ -1,5 +1,24 @@
 package algorithm
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class QuickSortTest
+internal class QuickSortTest {
+    private val alg = QuickSort()
+
+    @Test
+    fun `should sort full input`() {
+        val input = arrayOf(3, 7, 1, 8, 14, 23, 4, 2, 8, 2)
+        alg.quickSort(input)
+
+        assertArrayEquals(arrayOf(1, 2, 2, 3, 4, 7, 8, 8, 14, 23), input)
+    }
+
+    @Test
+    fun `should sort selected part of input`() {
+        val input = arrayOf(3, 7, 1, 8, 14, 23, 4, 2, 8, 2)
+        alg.quickSort(input, 3,8)
+
+        assertArrayEquals(arrayOf(3,7,1,2,4,8,8,14,23,2), input)
+    }
+}
