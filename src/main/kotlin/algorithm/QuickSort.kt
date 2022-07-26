5c35e8ad698580e7434b289ad5412d33a0083ed4
Anton Marin <antonaryo85@gmail.com>
2022-07-26 08:25:17 +0300

    formatting

--- src/main/kotlin/algorithm/QuickSort.kt
+++ src/main/kotlin/algorithm/QuickSort.kt
@@ -9,41 +9,41 @@ class QuickSort {
      * @param leftIndex Индекс первого сортируемого элемента массива
      * @param rightIndex Индекс последнего сортируемого элемента массива
      */
-    fun quickSort(input: Array<Int>, leftIndex: Int = 0, rightIndex: Int = input.size-1) {
-                if (input.isEmpty())
-                    return //завершить выполнение если длина массива равна 0
-         
-                if (leftIndex >= rightIndex)
-                    return //завершить выполнение если уже нечего делить
-         
-                // выбрать опорный элемент
-                val middleIndex = leftIndex + (rightIndex - leftIndex) / 2
-                val baseValue = input[middleIndex]
-         
-                // разделить на подмассивы, который больше и меньше опорного элемента
-                var i = leftIndex
-                var j = rightIndex
-                while (i <= j) {
-                        while (input[i] < baseValue) {
-                            i++
-                        }
-             
-                        while (input[j] > baseValue) {
-                            j--
-                        }
-             
-                        if (i <= j) { //меняем местами
-                            input[i] = input[j].also { input[j] = input[i] }
-                            i++
-                            j--
-                        }
-                    }
-         
-                // вызов рекурсии для сортировки левой и правой части
-                if (leftIndex < j)
-                    quickSort(input, leftIndex, j)
-         
-                if (rightIndex > i)
-                    quickSort(input, i, rightIndex)
+    fun quickSort(input: Array<Int>, leftIndex: Int = 0, rightIndex: Int = input.size - 1) {
+        if (input.isEmpty())
+            return //завершить выполнение если длина массива равна 0
+
+        if (leftIndex >= rightIndex)
+            return //завершить выполнение если уже нечего делить
+
+        // выбрать опорный элемент
+        val middleIndex = leftIndex + (rightIndex - leftIndex) / 2
+        val baseValue = input[middleIndex]
+
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        var i = leftIndex
+        var j = rightIndex
+        while (i <= j) {
+            while (input[i] < baseValue) {
+                i++
             }
+
+            while (input[j] > baseValue) {
+                j--
+            }
+
+            if (i <= j) { //меняем местами
+                input[i] = input[j].also { input[j] = input[i] }
+                i++
+                j--
+            }
+        }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (leftIndex < j)
+            quickSort(input, leftIndex, j)
+
+        if (rightIndex > i)
+            quickSort(input, i, rightIndex)
+    }
 }
