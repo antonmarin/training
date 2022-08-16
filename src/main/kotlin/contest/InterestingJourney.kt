d9202b7d41326a96f9eb426504cb56f0efd1ca70
Anton Marin <antonaryo85@gmail.com>
2022-08-16 09:14:06 +0300

    interesting journey

---
+++ src/main/kotlin/contest/InterestingJourney.kt
@@ -0,0 +1,83 @@
+package contest
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.math.abs
+
+// WA in test 15
+val input = BufferedReader(InputStreamReader(System.`in`))
+fun main() {
+    val inputData = parseInput(input)
+    val graph = Graph(inputData.cities, inputData.maxDistance)
+    println(graph.bfs(inputData.fromCityId, inputData.toCityId))
+}
+
+
+data class City(val cityId: Int, val x: Int, val y: Int)
+data class InputData(
+    val cities: List<City>,
+    val maxDistance: Int,
+    val fromCityId: CityId,
+    val toCityId: CityId,
+)
+typealias CityId = Int
+
+fun countDistanceBetween(cityFrom: City, cityTo: City): Int {
+    return abs(cityTo.x - cityFrom.x) + abs(cityTo.y - cityFrom.y)
+}
+
+fun parseInput(reader: BufferedReader): InputData {
+    val citiesCount = reader.readLine()!!.toInt()
+    val cities = mutableListOf<City>()
+    for (i in 1..citiesCount) {
+        cities.add(reader.readLine()!!.split(' ').let { City(i, it[0].toInt(), it[1].toInt()) })
+    }
+    val maxDistance = reader.readLine()!!.toInt()
+    val (fromCity, toCity) = reader.readLine()!!.split(' ').map { it.toInt() }
+
+    return InputData(cities, maxDistance, fromCity, toCity)
+}
+
+class Graph(private val cities: List<City>, maxDistance: Int) {
+    val edges: Map<CityId, List<CityId>>
+
+    init {
+        val edgesFromTo = HashMap<CityId, MutableList<CityId>>()
+        cities.forEach { fromCity ->
+            cities.forEach { toCity ->
+                if (fromCity != toCity && countDistanceBetween(fromCity, toCity) <= maxDistance) {
+                    if (edgesFromTo[fromCity.cityId] == null) {
+                        edgesFromTo[fromCity.cityId] = mutableListOf()
+                    }
+                    edgesFromTo[fromCity.cityId]!!.add(toCity.cityId)
+                }
+            }
+        }
+        edges = edgesFromTo
+    }
+
+    fun bfs(fromCityId: CityId, toCityId: CityId): Int {
+        val queue: Queue<CityId> = LinkedList()
+        val visited = mutableSetOf<CityId>()
+        visited.add(fromCityId)
+        val roadsCntTo = HashMap<CityId,Int>()
+        roadsCntTo[fromCityId] = 0
+
+        var current: CityId? = fromCityId
+        while (current != null) {
+            if(current == toCityId) return roadsCntTo[current]!!
+            edges[current]?.forEach { target ->
+                if (target !in visited) {
+                    roadsCntTo[target] = roadsCntTo[current]!! + 1
+                    queue.add(target)
+                    visited.add(target)
+                }
+            }
+            current = queue.poll()
+        }
+
+        return -1
+    }
+}
---
+++ src/test/kotlin/contest/InterestingJourneyTest.kt
@@ -0,0 +1,144 @@
+package contest
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.BufferedReader
+import java.io.StringReader
+import java.util.stream.Stream
+
+internal class InterestingJourneyTest {
+    @Test
+    fun `should count abs of coordinates diff`() {
+        val cityA = City(1, 3, -8)
+        val cityB = City(2, -3, 2)
+
+        assertEquals(16, countDistanceBetween(cityA, cityB))
+    }
+
+    @Test
+    fun `should parse input`() {
+        val input = BufferedReader(StringReader(INPUT))
+
+        val result = parseInput(input)
+
+        assertEquals(
+            listOf(
+                City(1, 0, 0),
+                City(2, 0, 2),
+                City(3, 2, 2),
+                City(4, 0, -2),
+                City(5, 2, -2),
+                City(6, 2, -1),
+                City(7, 2, 1),
+            ),
+            result.cities,
+        )
+        assertEquals(2, result.maxDistance)
+        assertEquals(1, result.fromCityId)
+        assertEquals(3, result.toCityId)
+    }
+
+    @Test
+    fun `should build graph without edges longer then max`() {
+        val cities = listOf(
+            City(1, 0, 0),
+            City(2, 0, 2),
+            City(3, 2, 0),
+            City(4, 0, 3),
+        )
+
+        val graph = Graph(cities, 2)
+
+        assertEquals(
+            mapOf(
+                1 to listOf(2, 3),
+                2 to listOf(1, 4),
+                3 to listOf(1),
+                4 to listOf(2),
+            ),
+            graph.edges
+        )
+    }
+
+    @Test
+    fun `should count edges`() {
+        val cities = listOf(
+            City(1, 0, 0),
+            City(2, 0, 2),
+            City(3, 2, 0),
+            City(4, 0, 3),
+        )
+
+        val graph = Graph(cities, 2)
+
+        assertEquals(2, graph.bfs(1, 4))
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBfsExpectations")
+    fun `should count edges by input`(expected: Int, input: String) {
+        val inputData = parseInput(BufferedReader(StringReader(input)))
+
+        val graph = Graph(inputData.cities, inputData.maxDistance)
+
+        assertEquals(expected, graph.bfs(inputData.fromCityId, inputData.toCityId))
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideBfsExpectations(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                2, """
+                7
+                0 0
+                0 2
+                2 2
+                0 -2
+                2 -2
+                2 -1
+                2 1
+                2
+                1 3
+            """.trimIndent()
+            ),
+            Arguments.of(
+                1, """
+                4
+                0 0
+                1 0
+                0 1
+                1 1
+                2
+                1 4
+            """.trimIndent()
+            ),
+            Arguments.of(
+                -1, """
+                4
+                0 0
+                2 0
+                0 2
+                2 2
+                1
+                1 4
+            """.trimIndent()
+            ),
+        )
+
+        private val INPUT = """
+            7
+            0 0
+            0 2
+            2 2
+            0 -2
+            2 -2
+            2 -1
+            2 1
+            2
+            1 3
+        """.trimIndent()
+    }
+}
